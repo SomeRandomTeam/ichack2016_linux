0f7fd6ab497282b5f90c2be6277f0943e13cc94f
Szilveszter Székely <szekelyszilv@gmail.com>
2016-02-21 05:28:38 +0000

    Implement screen grabbing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('use-strict')
 require('dotenv').load()
 
 var http = require('http')
+var spawn = require('child_process').spawn
 
 var express = require('express')
 
@@ -11,5 +12,19 @@ var server = http.Server(app)
 module.exports = server
 
 app.set('port', process.env.PORT)
+
+app.get('/', function (req, res) {
+  var grab = spawn('ffmpeg', [
+    '-f', 'x11grab',
+    '-r', '25',
+    '-s', '1366x768',
+    '-i', ':0.0',
+    '-vframes', '1',
+    '-f', 'mjpeg',
+    '-'
+  ])
+
+  grab.stdout.pipe(res)
+})
 
 server.listen(9999)
